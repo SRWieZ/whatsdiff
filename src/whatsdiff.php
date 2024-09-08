24623b56fac8436fca5a0938451ee2931d68e478
Eser DENIZ <srwiez@gmail.com>
2024-09-08 18:01:55 +0200

    Base diff functions

--- src/whatsdiff.php
+++ src/whatsdiff.php
@@ -1,19 +1,26 @@
 <?php
 
-error_reporting(0);
 
 
 if (! class_exists('\Composer\InstalledVersions')) {
     require __DIR__.'/../vendor/autoload.php';
 }
 
+error_reporting(0);
+(new \NunoMaduro\Collision\Provider())->register();
+
 // Argument parsing
 $argv = array_slice($argv, 1); // Remove script name
 $options['show_version'] = false;
+$options['ignore_last'] = false;
 
 foreach ($argv as $key => $arg) {
     if (in_array($arg, ['-V', '--version'])) {
         $options['show_version'] = true;
+        unset($argv[$key]);
+    }
+    if (in_array($arg, ['--ignore-last'])) {
+        $options['ignore_last'] = true;
         unset($argv[$key]);
     }
 }
@@ -29,6 +36,13 @@ function showHelp(): void
     echo 'Usage: whatsdiff'.PHP_EOL;
     echo PHP_EOL;
     echo "See what's changed in your project's dependencies".PHP_EOL;
+    echo PHP_EOL;
+    echo 'Options:'.PHP_EOL;
+    echo '  -V, --version       Show app versions'.PHP_EOL;
+    echo '      --ignore-last   Ignore last uncommited changes'.PHP_EOL;
+    echo PHP_EOL;
+    echo 'Commands:'.PHP_EOL;
+    echo '  help                Show this help information'.PHP_EOL;
     echo PHP_EOL;
     echo 'Informations:'.PHP_EOL;
     echo ! str_starts_with($version, '@git_tag') ? '  Version: '.$version.PHP_EOL : '';
@@ -64,12 +78,72 @@ function getFileContentOfCommit(string $filename, string $commitHash): string
     return shell_exec("git show $commitHash:$filename");
 }
 
-dump(gitLogOfFile('composer.lock'));
-dump(isFileHasBeenRecentlyUpdated('composer.lock'));
-dump(mb_strlen(getFileContentOfCommit('composer.lock', gitLogOfFile('composer.lock')[0])));
+function getFilesToCompare(string $filename, array $commitLogs, bool $recentlyUpdated): array
+{
+    $last = $recentlyUpdated ? file_get_contents($filename) : getFileContentOfCommit($filename, $commitLogs[0]);
 
+    $previousHashKey = $recentlyUpdated ? 0 : 1;
+
+    if (! isset($commitLogs[$previousHashKey])) {
+        throw new Exception('Could not found any previous changes in '.$filename);
+    }
+
+    $previous = getFileContentOfCommit($filename, $commitLogs[$previousHashKey]);
+
+    return [$last, $previous];
+}
+
+function extractComposerPackagesVersions($composerLockContent): array
+{
+    return collect($composerLockContent['packages'])
+        ->merge($composerLockContent['packages-dev'])
+        ->mapWithKeys(fn ($package) => [$package['name'] => $package['version']])
+        ->toArray();
+}
+
+function diffComposerLockPackages($last, $previous)
+{
+    $last = json_decode($last, associative: true);
+    $previous = json_decode($previous, associative: true);
+
+    $last = extractComposerPackagesVersions($last);
+    $previous = extractComposerPackagesVersions($previous);
+
+    $diff = collect($previous)
+        ->mapWithKeys(fn ($version, $name) => [
+            $name => [
+                'from' => $version,
+                'to'   => $last[$name] ?? null,
+            ]
+        ]);
+
+    $newPackages = collect($last)
+        ->diffAssoc($previous)
+        ->mapWithKeys(fn ($version, $name) => [
+            $name => [
+                'from' => null,
+                'to'   => $version,
+            ]
+        ]);
+
+
+    return $diff->merge($newPackages)
+        ->filter(fn ($el) => $el['from'] !== $el['to'])
+        ->toArray();
+}
 
 if ($command === 'help' || $options['show_version']) {
     showHelp();
     exit;
 }
+
+
+$filename = 'composer.lock';
+
+$commitLogs = gitLogOfFile($filename);
+
+$recentlyUpdated = ! $options['ignore_last'] && isFileHasBeenRecentlyUpdated($filename);
+
+[$last, $previous] = getFilesToCompare($filename, $commitLogs, $recentlyUpdated);
+
+dump(diffComposerLockPackages($last, $previous));
