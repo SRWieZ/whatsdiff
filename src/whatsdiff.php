01f2a58a504d11095b7d66eeaf48ce98f7300aac
Eser DENIZ <srwiez@gmail.com>
2024-09-08 17:08:24 +0200

    First functions

--- src/whatsdiff.php
+++ src/whatsdiff.php
@@ -3,7 +3,7 @@
 error_reporting(0);
 
 
-if ( ! class_exists('\Composer\InstalledVersions')) {
+if (! class_exists('\Composer\InstalledVersions')) {
     require __DIR__.'/../vendor/autoload.php';
 }
 
@@ -37,8 +37,39 @@ function showHelp(): void
     echo php_sapi_name() == 'micro' ? '  Compiled with https://github.com/crazywhalecc/static-php-cli'.PHP_EOL : '';
 }
 
+function gitLogOfFile(string $filename): array
+{
+    $output = shell_exec("git log --pretty=format:'%h' $filename");
+
+    return explode("\n", $output);
+}
+
+function isFileHasBeenRecentlyUpdated(string $filename): bool
+{
+    // Execute the command and get the output
+    $output = shell_exec("git status --porcelain");
+
+    $status = collect(explode("\n", trim($output)))
+        ->mapWithKeys(function ($line) {
+            $line = array_values(array_filter(explode(' ', $line)));
+
+            return [$line[1] => $line[0]];
+        });
+
+    return $status->get($filename) == 'M';
+}
+
+function getFileContentOfCommit(string $filename, string $commitHash): string
+{
+    return shell_exec("git show $commitHash:$filename");
+}
+
+dump(gitLogOfFile('composer.lock'));
+dump(isFileHasBeenRecentlyUpdated('composer.lock'));
+dump(mb_strlen(getFileContentOfCommit('composer.lock', gitLogOfFile('composer.lock')[0])));
+
+
 if ($command === 'help' || $options['show_version']) {
     showHelp();
     exit;
 }
-
